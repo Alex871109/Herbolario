3635c0ead07aa0eae8a0cb495a285cd6b305a0f2
alex <alex87gonz11@gmail.com>
2022-10-30 10:38:08 +0100

    API Usos (Index, metodo object_toarray groups)

---
+++ .history/src/Controller/Api/UsosController_20221029082806.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Usos;
+use App\Repository\UsosRepository;
+use App\Services\Manager;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\JsonDecoder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
+#[Route('/Api/usos')]
+class UsosController extends AbstractController
+{
+    #[Route('/index', name: 'api_usos_index', methods: ['GET'])]
+    public function index(UsosRepository $usosRepository, Manager $manager): JsonResponse
+    {
+        $normalizer = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizer, []);
+        $usos=$usosRepository->findAll();
+        dump($usos);
+       // $usos_array= $manager->object_to_array($usos);
+       
+        dump($usos_array);
+        die();
+        return $this->json(['usos'=>$usos_array],200);
+    }
+
+    #[Route('/edit_get/{id}',name:'api_edit_get_herbolario',methods:['GET', 'POST'])]
+    public function edit_get(Request $request,int $id,EntityManagerInterface $entityManager, Manager $manager, UsosRepository $herbolarioRepository): JsonResponse
+    {
+        $herbolario=$herbolarioRepository->findOneById($id);
+        if(!$herbolario)
+            throw $this->createNotFoundException('Herbolario not found');
+        $dataResponse=['herbolario'=>$manager->object_to_array($herbolario)];
+
+       if($request->getMethod()==='POST'){
+            $data_received=json_Decode($request->getContent()); //$request->getContent devuelve en 1 json el contenido del post
+            $save_operation=$manager->save($data_received,$herbolario);
+            if($save_operation['error'])
+                $dataResponse = ['status' => 500, 'response' => 'fail','method'=>'POST', 'herbolario' => null];
+            else
+                $dataResponse=['status'=>200,'response'=>'success', 'method'=>'POST','herbolario'=>$manager->object_to_array($save_operation['entity'])];
+  
+        }
+
+            return $this->json($dataResponse,200);
+        
+    }
+
+    // #[Route('/delete/{id}', name: 'api_herbolario_delete', methods: ['DELETE'])]
+    // public function delete(UsosRepository $herbolarioRepository, Manager $manager, int $id): JsonResponse
+    // {
+    //     // $herbolario=$herbolarioRepository->findOneById($id);
+    //     // if(!$herbolario)
+    //     //     throw $this->createNotFoundException('Herbolario not found');
+        
+    //     // try {
+    //     //     $infocomercial_herbolario=$infocomercialRepository->findByHerbolarioid($herbolario);
+    //     //     foreach($infocomercial_herbolario as $herbolario_row)
+    //     //         $manager->delete($herbolario_row,$infocomercialRepository);
+    //     //     $manager->delete($herbolario,$herbolarioRepository);
+    //     //     $status = 200;
+    //     //     $response = 'success';
+    //     // } catch (\Exception $exception) {
+    //     //     $status = 500;
+    //     //     $response = $exception->getMessage();
+    //     // }
+    //     // return $this->json([
+    //     //     'status' => $status,
+    //     //     'response' => $response,
+    //     // ], 200);
+        
+    // }
+
+
+    #[Route('/new', name: 'api_herbolario_new', methods: ['POST'])]
+    public function new(UsosRepository $herbolarioRepository,Request $request, Manager $manager): JsonResponse
+    {
+        $herbolario=new Usos();
+        $data_received=json_Decode($request->getContent());
+        $save_operation=$manager->save($data_received,$herbolario);
+            if($save_operation['error'])
+                $dataResponse = ['status' => 500, 'response' => 'fail'];
+            else
+                $dataResponse = ['status' => 200, 'response' => 'success', 'new herbolario'=> $save_operation['entity']];
+
+        return $this->json($dataResponse,200);
+    }
+
+}
---
+++ .history/src/Entity/Herbolario_20221030101321.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+use App\Repository\HerbolarioRepository;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Herbolario
+ *
+ * @ORM\Table(name="herbolario")
+ * @ORM\Entity(repositoryClass=HerbolarioRepository::Class)
+ */
+class Herbolario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ID", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="Nombre", type="string", length=100, nullable=true)
+     * @Groups("basic")
+     */
+    private $nombre;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="URL", type="string", length=255, nullable=true)
+     * @Groups("basic")
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(?string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+
+        return $this->nombre;
+    }
+
+
+}
